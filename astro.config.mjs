5a1ea9b137e0d219fafc169860fbe55565bf9e00
wolf-yuan <h1030035@gmail.com>
2023-10-14 22:48:14 +0800

    Use cloudflare pages instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), react()],
-  adapter: vercel({
-    analytics: true
-  }),
-  output: "server"
+  adapter: cloudflare(),
+  output: "server",
 });
