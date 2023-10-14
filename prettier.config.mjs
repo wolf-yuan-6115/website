2919b20075a3c312286bb2eda6182f93f9a2a8cb
wolf-yuan <h1030035@gmail.com>
2023-10-15 00:13:23 +0800

    its working ig

---
+++ prettier.config.mjs
@@ -0,0 +1,22 @@
+export default {
+  "printWidth": 70,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": false,
+  "trailingComma": "all",
+  "bracketSpacing": true,
+  "arrowParens": "always",
+  "requirePragma": false,
+  "insertPragma": false,
+  "proseWrap": "never",
+  "plugins": ["prettier-plugin-astro"],
+  "overrides": [
+    {
+      "files": "*.astro",
+      "options": {
+        "parser": "astro",
+      },
+    },
+  ],
+}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,47 @@
-
+import { StrictMode, useEffect, useState } from "react";
 
 export default function Nav() {
-  return <nav className="sticky top-0 backdrop-blur-md backdrop-brightness-75">
-  <div className="mx-auto max-w-5xl py-2">
-    <div className="grid grid-cols-2 py-2 place-items-center">
-      <div>
-        <a
-          href="https://gitlab.com/wolf-yuan"
-          className="text-white tabs font-semibold hover:font-bold hover:bg-gray-900"
-          >GitLab</a
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 56);
+    });
+  });
+
+  return (
+    <StrictMode>
+      <nav
+        className={
+          (scroll ? "backdrop-brightness-75" : "") +
+          " sticky top-0 backdrop-blur-md duration-300"
+        }
+      >
+        <div
+          className={
+            (scroll ? "py-2" : "py-5") +
+            " mx-auto max-w-5xl duration-300"
+          }
         >
-      </div>
-      <div>
-        <a
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          className="text-white tabs font-semibold hover:font-bold">YouTube</a
-        >
-      </div>
-    </div>
-  </div>
-</nav>
+          <div className="grid grid-cols-2 py-2 place-items-center">
+            <div>
+              <a
+                href="https://gitlab.com/wolf-yuan"
+                className="text-white tabs font-semibold hover:font-bold hover:bg-gray-900"
+              >
+                GitLab
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                className="text-white tabs font-semibold hover:font-bold"
+              >
+                YouTube
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </StrictMode>
+  );
 }
-
-
