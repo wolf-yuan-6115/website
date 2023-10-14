60d3bfece828eaceaceebbe6835c902c33f73bd1
wolf-yuan <h1030035@gmail.com>
2023-10-15 00:38:23 +0800

    I got image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { StrictMode, useEffect, useState } from "react";
 
-export default function Nav() {
+export default function Nav({ children }) {
   const [scroll, setScroll] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -18,27 +18,12 @@ export default function Nav() {
       >
         <div
           className={
-            (scroll ? "py-2" : "py-5") +
-            " mx-auto max-w-5xl duration-300"
+            (scroll ? "py-3" : "py-5") +
+            " mx-auto max-w-4xl duration-300"
           }
         >
-          <div className="grid grid-cols-2 py-2 place-items-center">
-            <div>
-              <a
-                href="https://gitlab.com/wolf-yuan"
-                className="text-white tabs font-semibold hover:font-bold hover:bg-gray-900"
-              >
-                GitLab
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                className="text-white tabs font-semibold hover:font-bold"
-              >
-                YouTube
-              </a>
-            </div>
+          <div className="grid grid-cols-4 gap-4">
+            <div className="col-start-1 col-end-2">{children}</div>
           </div>
         </div>
       </nav>
