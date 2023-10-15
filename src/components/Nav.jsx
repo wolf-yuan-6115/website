fa658e4f7ca0f011d4f3282d2669cf886208c849
wolf-yuan <h1030035@gmail.com>
2023-10-15 19:55:25 +0800

    Change to twMerge and add placeholder text

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { SiGitlab, SiYoutube } from "@icons-pack/react-simple-icons";
+import { twMerge } from "tailwind-merge";
 
 export default function Nav({ children }) {
   const [scroll, setScroll] = useState(false);
@@ -11,26 +12,26 @@ export default function Nav({ children }) {
 
   return (
     <nav
-      className={
-        (scroll ? "backdrop-brightness-75" : "") +
-        " sticky top-0 backdrop-blur-md duration-300"
-      }
+      className={twMerge(
+        "sticky top-0 backdrop-blur-md duration-300",
+        scroll && "backdrop-brightness-75",
+      )}
     >
       <div
-        className={
-          (scroll ? "py-4" : "py-6") +
-          " px-5 mx-auto max-w-4xl duration-300"
-        }
+        className={twMerge(
+          "px-5 mx-auto max-w-4xl duration-300",
+          scroll ? "py-4" : "py-6",
+        )}
       >
         <div className="grid grid-cols-2">
           <div className="col-start-1 col-end-2">
             <div className="flex flex-row items-center gap-4">
               <div className="basis-1/8">{children}</div>
               <div
-                className={
-                  "basis-1/8 font-bold text-xl transition-opacity duration-300 " +
-                  (scroll ? "opacity-100" : "opacity-0")
-                }
+                className={twMerge(
+                  "basis-1/8 font-bold text-xl transition-opacity duration-300",
+                  scroll ? "opacity-100" : "opacity-0",
+                )}
               >
                 Wolf Yuan
               </div>
