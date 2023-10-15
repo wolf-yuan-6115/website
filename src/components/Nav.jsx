5c56656a6ce528e2c1057146d96b741ffa753e8c
wolf-yuan <h1030035@gmail.com>
2023-10-15 18:06:26 +0800

    Update links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { SiGitlab, SiYoutube } from "@icons-pack/react-simple-icons";
 
 export default function Nav({ children }) {
   const [scroll, setScroll] = useState(false);
@@ -21,13 +22,13 @@ export default function Nav({ children }) {
           " px-5 mx-auto max-w-4xl duration-300"
         }
       >
-        <div className="grid grid-cols-2 text-xl">
+        <div className="grid grid-cols-2">
           <div className="col-start-1 col-end-2">
             <div className="flex flex-row items-center gap-4">
               <div className="basis-1/8">{children}</div>
               <div
                 className={
-                  "basis-1/8 transition-opacity duration-300 " +
+                  "basis-1/8 font-bold text-xl transition-opacity duration-300 " +
                   (scroll ? "opacity-100" : "opacity-0")
                 }
               >
@@ -36,9 +37,23 @@ export default function Nav({ children }) {
             </div>
           </div>
           <div className="col-start-2 col-end-3">
-            <div className="flex flex-row justify-end items-center gap-4 h-10">
-              <div className="basis-1/8">Blog</div>
-              <div className="basis-1/8">YouTube</div>
+            <div className="flex flex-row justify-end items-center gap-6 text-lg h-10">
+              <div className="basis-1/10">
+                <a
+                  href="https://gitlab.com/wolf-yuan"
+                  target="_blank"
+                >
+                  <SiGitlab />
+                </a>
+              </div>
+              <div className="basis-1/10">
+                <a
+                  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                  target="_blank"
+                >
+                  <SiYoutube />
+                </a>
+              </div>
             </div>
           </div>
         </div>
