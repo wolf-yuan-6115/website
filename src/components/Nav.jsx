f71b83136eef0aa6b3760e6c8e9906d68f83d0d9
wolf-yuan <h1030035@gmail.com>
2023-10-15 17:39:27 +0800

    Animated title

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Nav({ children }) {
   const [scroll, setScroll] = useState(false);
@@ -9,24 +9,40 @@ export default function Nav({ children }) {
   });
 
   return (
-    <StrictMode>
-      <nav
+    <nav
+      className={
+        (scroll ? "backdrop-brightness-75" : "") +
+        " sticky top-0 backdrop-blur-md duration-300"
+      }
+    >
+      <div
         className={
-          (scroll ? "backdrop-brightness-75" : "") +
-          " sticky top-0 backdrop-blur-md duration-300"
+          (scroll ? "py-4" : "py-6") +
+          " px-5 mx-auto max-w-4xl duration-300"
         }
       >
-        <div
-          className={
-            (scroll ? "py-3" : "py-5") +
-            " mx-auto max-w-4xl duration-300"
-          }
-        >
-          <div className="grid grid-cols-4 gap-4">
-            <div className="col-start-1 col-end-2">{children}</div>
+        <div className="grid grid-cols-2 text-xl">
+          <div className="col-start-1 col-end-2">
+            <div className="flex flex-row items-center gap-4">
+              <div className="basis-1/8">{children}</div>
+              <div
+                className={
+                  "basis-1/8 transition-opacity duration-300 " +
+                  (scroll ? "opacity-100" : "opacity-0")
+                }
+              >
+                Wolf Yuan
+              </div>
+            </div>
+          </div>
+          <div className="col-start-2 col-end-3">
+            <div className="flex flex-row justify-end items-center gap-4 h-10">
+              <div className="basis-1/8">Blog</div>
+              <div className="basis-1/8">YouTube</div>
+            </div>
           </div>
         </div>
-      </nav>
-    </StrictMode>
+      </div>
+    </nav>
   );
 }
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "'JetBrains Mono Variable'",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
