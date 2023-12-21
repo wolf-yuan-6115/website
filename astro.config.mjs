b94ab11e60e1ad4283472c92c3d0da8ceb61b3ed
wolf-yuan <h1030035@gmail.com>
2023-12-21 21:21:29 +0800

    Bump packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
-import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -9,7 +8,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wolf-yuan.dev",
-  integrations: [mdx(), tailwind(), react(), sitemap(), prefetch()],
+  integrations: [mdx(), tailwind(), react(), sitemap()],
   adapter: cloudflare(),
   output: "server",
   image: {
