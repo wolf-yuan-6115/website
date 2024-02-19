ce60590a6cad6afe1f54a741cc5f8ca79fb5efea
wolf-yuan <h1030035@gmail.com>
2024-02-19 22:14:26 +0800

    Add IPFS builder config

---
+++ ipfs.config.mjs
@@ -0,0 +1,12 @@
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://wolf-yuan.dev",
+  integrations: [mdx(), tailwind(), react(), sitemap()],
+  output: "static",
+});
