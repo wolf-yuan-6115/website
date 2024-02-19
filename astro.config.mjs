964d05e286b6f9c5af6648ee738b67911c552684
wolf-yuan <h1030035@gmail.com>
2024-02-20 00:00:18 +0800

    Update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,21 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://wolf-yuan.dev",
-  integrations: [mdx(), tailwind(), react(), sitemap()],
+  integrations: [mdx(), tailwind(), react(), sitemap(), icon({
+    include: {
+      "material-symbols": ["arrow-forward-rounded"]
+    }
+  })],
   adapter: cloudflare(),
   output: "server",
   image: {
     service: {
-      entrypoint: "astro/assets/services/noop",
-    },
-  },
+      entrypoint: "astro/assets/services/noop"
+    }
+  }
 });
