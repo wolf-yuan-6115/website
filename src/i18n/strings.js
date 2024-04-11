02d002343527650da44743d5741026ae26d3fac1
wolf-yuan <h1030035@gmail.com>
2024-04-11 21:18:07 +0800

    Fix typo in variable name

--- src/i18n/strings.js
+++ src/i18n/strings.js
@@ -33,14 +33,14 @@ export default {
       title: "JavaScript is disabled",
       description:
         "JavaScript is disabled on this website, some feature or styling might break.",
-      addtional:
+      additional:
         "This site uses Google Analysis with Cloudflare Zaraz, consider using extensions like uBlock Origin to block tracking instead disabling script.",
     },
     "zh-tw": {
       title: "JavaScript 已停用",
       description:
         "JavaScript 已在這個網頁上停用，部分功能或是樣式可能會無法成功顯示",
-      addtional:
+      additional:
         "我的網站使用 Cloudflare Zaraz 與 Google Analysis，您可以嘗試使用類似 uBlock Origin 的擴充功能封鎖追蹤，而不是停用 JavaScript",
     },
   },
